6a1ef0f75ff983279a22e315aff44a80ed8d51ed
Tony Lau <99046505+intrepidcanadian@users.noreply.github.com>
2024-10-02 15:32:18 +0800

    latest

--- constant/constant.js
+++ constant/constant.js
@@ -8,7 +8,6 @@ const usdcContractAddress = "0x349298B0E20DF67dEFd6eFb8F3170cF4a32722EF";
 
 const usdtContractAddress = "0x7d682e65EFC5C13Bf4E394B8f376C48e6baE0355";
 
-
 module.exports = {
   poolContractAddress,
   nonFungiblePositionManagerContractAddress,
